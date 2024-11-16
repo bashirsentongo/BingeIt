b009a592a89b5d90c3d4b0b8ba3eba8c0d78b8e9
Bashir <bashirsk23@gmail.com>
2024-11-16 16:47:19 +0000

    - Added Fastlane to project

---
+++ BingeIt/BingeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingeItApp.swift
+//  BingeIt
+//
+//  Created by Bashir Sentongo on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BingeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BingeItTests/BingeItTests.swift
@@ -0,0 +1,16 @@
+//
+//  BingeItTests.swift
+//  BingeItTests
+//
+//  Created by Bashir Sentongo on 16/11/2024.
+//
+
+import Testing
+
+struct BingeItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
